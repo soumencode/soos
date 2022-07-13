3480010b66a1ac12a50a7fb248d19e02706ee866
soumencode <soumencode@gmail.com>
2022-07-14 01:03:49 +0900

    setup base for qemu-virt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+#![feature(naked_functions)]
+
+use core::arch::asm;
+
+#[naked]
+#[export_name = "_start"]
+extern "C" fn _start() {
+    unsafe {
+        asm!("j main",
+			 options(noreturn));
+    }
+}
+
+/* https://www.reddit.com/r/rust/comments/estvau/til_why_the_eh_personality_language_item_is */
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+/* https://docs.rust-embedded.org/book/start/panicking.html */
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+fn main() -> ! {
+    loop {}
 }
